febc4fd001f8e7e31a1bda667e3628640181fda8
guillermotoppph <guillermotoppph@gmail.com>
2024-04-27 14:59:50 -0300

    Version 2.0 - Funcional

--- script.js
+++ script.js
@@ -3,19 +3,30 @@ function playSonido(idElementoAudio){
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
-let contador=0;
 
-while  (contador < 9){
-        const tecla= listaDeTeclas[contador];
-        const instrumento = tecla.classList[1];
-        
-        const idAudio = `#sonido_${instrumento}`;
-        console.log(idAudio);
-        
-        tecla.onclick = function (){
-            playSonido(idAudio);
-        } ;
 
-        contador = contador +1;        
-}
+    for (let contador = 0; contador < listaDeTeclas.length; contador++ ){
+            const tecla= listaDeTeclas[contador];
+            const instrumento = tecla.classList[1];
+            
+            const idAudio = `#sonido_${instrumento}`;
+            console.log(idAudio);
+            
+                tecla.onclick = function(){
+                    playSonido(idAudio);
+                }; 
+            tecla.onKeyDown = function(evento){
+                console.log(evento.code == 'Space');
+                tecla.classList.add('activa')
+                if (evento.code === 'Space' || evento.code === 'Enter'){
+                    tecla.classList.add('activa');
+                }
+                console.log((evento.code === 'Space' || evento.code === 'Enter'))
+
+            };
+            tecla.onKeyUp = function(){
+                tecla.classList.remove('activa')
+            }
+        
+    }
 
