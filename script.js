0a4b98bb3eb7e56a3d1792cb46e2962d0d95d3f0
guillermotoppph <guillermotoppph@gmail.com>
2024-04-29 17:30:44 -0300

    Version 2.3 - Funcional - Nuevo puntero - Nuevo Encabezado - Seccion Acerca de:

--- script.js
+++ script.js
@@ -1,32 +1,37 @@
-function playSonido(idElementoAudio){
-    document.querySelector(idElementoAudio ).play();
+function playSonido (idElementoAudio){
+    document.querySelector(idElementoAudio).play();
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
 
-    for (let contador = 0; contador < listaDeTeclas.length; contador++ ){
-            const tecla= listaDeTeclas[contador];
-            const instrumento = tecla.classList[1];
-            
-            const idAudio = `#sonido_${instrumento}`;
-            console.log(idAudio);
-            
-                tecla.onclick = function(){
-                    playSonido(idAudio);
-                }; 
-            tecla.onKeyDown = function(evento){
-                console.log(evento.code == 'Space');
-                tecla.classList.add('activa')
-                if (evento.code === 'Space' || evento.code === 'Enter'){
-                    tecla.classList.add('activa');
-                }
-                console.log((evento.code === 'Space' || evento.code === 'Enter'))
+for(let contador = 0; contador < listaDeTeclas.length; contador++){
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    console.log(contador)
 
-            };
-            tecla.onKeyUp = function(){
-                tecla.classList.remove('activa')
-            }
+    const idAudio = `#sonido_${instrumento}`;
+    console.log(idAudio)
+
+    tecla.onclick = function (){
+        playSonido(idAudio);
+    };
+    tecla.onkeydown = function(evento){
+        
+        if (evento.code === 'Space' || evento.code === 'Enter'){
+            tecla.classList.add('activa');
+        }
+        console.log(evento.code === 'Space' || evento.code === 'Enter')
+       
         
     }
+    tecla.onkeyup = function(){
+        tecla.classList.remove('activa');
+    }
+   
+
+}
+
+
+
 
